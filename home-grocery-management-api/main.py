b174512ca6d9fba454a12f0bbc7ec62a995f13b7
ashay <katreashay@gmail.com>
2024-10-24 01:40:32 -0400

    SQLAlchemy setup

--- home-grocery-management-api/main.py
+++ home-grocery-management-api/main.py
@@ -2,6 +2,20 @@ from fastapi import FastAPI, HTTPException
 from pydantic import BaseModel
 from typing import List, Optional
 from datetime import date
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from config import DATABASE_URL
+
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+Base = declarative_base()
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 app = FastAPI()
 
