ebba6c741602c0c14a82e8c8ab81c0efa3b4cca0
ashay <katreashay@gmail.com>
2024-10-26 19:56:12 -0400

    Implemented and deployed with Database setup

---
+++ home-grocery-management-api/schemas.py
@@ -0,0 +1,41 @@
+# schemas.py
+from pydantic import BaseModel, Field
+from datetime import date
+from typing import List
+from typing import Optional
+
+
+class GroceryItem(BaseModel):
+    id: Optional[int]
+    name: str
+    category: str
+    quantity: str
+    expirationDate: date = Field(..., alias="expiration_date")
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+        populate_by_name = True
+
+class Alert(BaseModel):
+    id: int
+    name: str
+    category: str
+    expirationDate: date
+    daysLeft: int
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
+
+class BatchItems(BaseModel):
+    items: List[GroceryItem]
+
+class ShoppingList(BaseModel):
+    name: str
+    category: str
+    neededQuantity: str
+
+    class Config:
+        orm_mode = True
+        from_attributes = True
